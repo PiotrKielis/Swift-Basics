7dcd1a90925d479cb79a1032c701103d8d01b117
PiotrKielis <119855230+PiotrKielis@users.noreply.github.com>
2025-06-30 09:57:29 +0200

    Update classesAndInheritance.swift

--- classesAndInheritance.swift
+++ classesAndInheritance.swift
@@ -78,3 +78,49 @@ print("Lion is tame? \(lion.isTame)")
 
 let persian = Persian(legs: 4, isTame: true)
 persian.speak()
+
+/*
+class Animal {
+    let legs: Int
+    init(legs: Int) {
+        self.legs = legs
+    }
+}
+class Dog: Animal {
+    func speak() {
+        print("Bark")
+    }
+}
+class Cat: Animal {
+    var isTame: Bool
+    init(legs: Int, isTame: Bool) {
+        self.isTame = isTame
+        super.init(legs: legs)
+    }
+    func speak() {
+        print("Meow")
+    }
+}
+class Corgi: Dog {
+    override func speak() {
+        print("Corgi bark")
+    }
+}
+class Poodle: Dog {
+    override func speak() {
+        print("Poodle wooof")
+    }
+}
+class Persian: Cat {
+    override func speak() {
+        print("Meow meow")
+    }
+}
+class Lion :Cat {
+    var isDirty: Bool
+    init(legs: Int, isTame: Bool, isDirty: Bool) {
+        self.isDirty = isDirty
+        super.init(legs: legs, isTame: isTame)
+    }
+}
+*/
